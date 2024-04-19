40806e9e0311e1000634e2f837d173a2edf3157a
Yaser <nightboy201@gmail.com>
2024-04-18 19:38:14 -0500

    Added threads locks

--- src/nimcls/di.nim
+++ src/nimcls/di.nim
@@ -1,4 +1,4 @@
-import tables
+import tables, locks
 import ./classobj
 
 const 
@@ -14,39 +14,62 @@ const
 
 type InjectionError* = object of ValueError
 
-var injectionsTblSingleton {.global.} = initTable[int, ClassObj]()
-var injectionsTbl {.global.} = initTable[int, proc(): ClassObj]()
-var staticInjectionsTblSingleton {.global.} = initTable[int, pointer]()
-var staticInjectionsTbl {.global.} = initTable[int, proc(): pointer]()
+## Injection & injectors tables
+var
+    injectionsTblSingleton {.global.} = initTable[int, ClassObj]()
+    injectionsTbl {.global.} = initTable[int, proc(): ClassObj]()
+    staticInjectionsTblSingleton {.global.} = initTable[int, pointer]()
+    staticInjectionsTbl {.global.} = initTable[int, proc(): pointer]()
+    classObjTblLock {.global.}: Lock
+    classStaticObjTblLock {.global.}: Lock
+    
+classObjTblLock.initLock
+classStaticObjTblLock.initLock
 
 
 proc copyStaticObject[T](obj: T) : pointer =
+    ## Copy object
+    ##
     let objSize = sizeof(T)
     let objPtr = alloc(objSize)
     copyMem(objPtr, addr obj, objSize)
     return objPtr
 
 proc addSingleton*[T : ClassObj | ClassStaticObj](obj : T) =
+    ## Adds a singleton object
+    ##
+    ## Adds a singleton object of type ClassObj or ClassStaticObj
+    ## to the injection table and uses its type as key for it.
+    ##
     when T is ClassObj and $ClassObj != $T:
-        if injectionsTbl.hasKey(T.signature):
-            raise newException(InjectionError, DUPLICATE_ERROR_1 & $T)
-        injectionsTblSingleton[T.signature] = obj
+        withLock classObjTblLock:
+            if injectionsTbl.hasKey(T.signature):
+                raise newException(InjectionError, DUPLICATE_ERROR_1 & $T)
+            injectionsTblSingleton[T.signature] = obj
     elif T is ClassStaticObj and $ClassStaticObj != $T:
-        if staticInjectionsTbl.hasKey(T.signature):
-            raise newException(InjectionError, DUPLICATE_ERROR_1 & $T)
-        if staticInjectionsTblSingleton.hasKey(T.signature) :
-            dealloc staticInjectionsTblSingleton[T.signature] 
-        staticInjectionsTblSingleton[T.signature] = copyStaticObject(obj)
+        withLock classStaticObjTblLock:
+            if staticInjectionsTbl.hasKey(T.signature):
+                raise newException(InjectionError, DUPLICATE_ERROR_1 & $T)
+            if staticInjectionsTblSingleton.hasKey(T.signature) :
+                dealloc staticInjectionsTblSingleton[T.signature] 
+            staticInjectionsTblSingleton[T.signature] = copyStaticObject(obj)
     else:
         raise newException(InjectionError, $T & SUBCLASS_ERROR)
 
 
 proc addSingleton*[T, R : ClassObj](clsDesc : typedesc[R], obj : T) =
+    ## Adds a singleton object
+    ##
+    ## Adds a singleton object of type ClassObj 
+    ## to the injection table and uses 
+    ## its type or its parent's class as key for it.
+    ##
     when $ClassObj != $T and $ClassObj != $R:
         if T is R :
-            if injectionsTbl.hasKey(R.signature):
-                raise newException(InjectionError, DUPLICATE_ERROR_1 & $R)
-            injectionsTblSingleton[R.signature] = obj
+            withLock classObjTblLock:
+                if injectionsTbl.hasKey(R.signature):
+                    raise newException(InjectionError, DUPLICATE_ERROR_1 & $R)
+                injectionsTblSingleton[R.signature] = obj
         else:
             raise newException(InjectionError, $T & SUBCLASS_1_ERROR & $R & SUBCLASS_2_ERROR)
     else:
@@ -54,27 +77,42 @@ proc addSingleton*[T, R : ClassObj](clsDesc : typedesc[R], obj : T) =
 
 
 proc addInjector*[T: ClassObj | ClassStaticObj](builder : proc(): T) =
+    ## Adds a procedure as an injector
+    ##
+    ## Adds a procedure that returns an object 
+    ## of type ClassObj or ClassStaticObj to the injectors table 
+    ## and uses the object's type as key for it. 
+    ##
     when T is ClassObj and $ClassObj != $T:
-        if injectionsTblSingleton.hasKey(T.signature):
-            raise newException(InjectionError, DUPLICATE_ERROR_2 & $T)
-        injectionsTbl[T.signature] = proc(): ClassObj = result = builder()
+        withLock classObjTblLock:
+            if injectionsTblSingleton.hasKey(T.signature):
+                raise newException(InjectionError, DUPLICATE_ERROR_2 & $T)
+            injectionsTbl[T.signature] = proc(): ClassObj = result = builder()
     elif T is ClassStaticObj and $ClassStaticObj != $T:
-        if staticInjectionsTblSingleton.hasKey(T.signature):
-            raise newException(InjectionError, DUPLICATE_ERROR_2 & $T)
-        staticInjectionsTbl[T.signature] 
-            = proc(): pointer =
-                var output = builder()
-                return addr(output)
+        withLock classStaticObjTblLock:
+            if staticInjectionsTblSingleton.hasKey(T.signature):
+                raise newException(InjectionError, DUPLICATE_ERROR_2 & $T)
+            staticInjectionsTbl[T.signature] 
+                = proc(): pointer =
+                    var output = builder()
+                    return addr(output)
     else:
         raise newException(InjectionError, $T & SUBCLASS_ERROR)
 
 
 proc addInjector*[T, R: ClassObj](clsDesc : typedesc[R], builder : proc(): T) =
+    ## Adds a procedure as an injector
+    ##
+    ## Adds a procedure that returns an object 
+    ## of type ClassObj to the injectors table 
+    ## it the object's type or its parent's class as key for it. 
+    ##
     when $ClassObj != $T and $ClassObj != $R:
         if T is R :
-            if injectionsTblSingleton.hasKey(R.signature):
-                raise newException(InjectionError, DUPLICATE_ERROR_2 & $R)
-            injectionsTbl[R.signature] = proc(): ClassObj = result = builder()
+            withLock classObjTblLock:
+                if injectionsTblSingleton.hasKey(R.signature):
+                    raise newException(InjectionError, DUPLICATE_ERROR_2 & $R)
+                injectionsTbl[R.signature] = proc(): ClassObj = result = builder()
         else:
             raise newException(InjectionError, $T & SUBCLASS_1_ERROR & $R & SUBCLASS_2_ERROR)
     else:
@@ -82,44 +120,63 @@ proc addInjector*[T, R: ClassObj](clsDesc : typedesc[R], builder : proc(): T) =
 
 
 proc inject*[T: ClassObj | ClassStaticObj](clsDesc : typedesc[T]) : T =
+    ## Returns an object 
+    ##
+    ## Returns an object of type ClassObj or ClassStaticObj 
+    ## which exists in the injection tables.
+    ##
     when T is ClassObj and $ClassObj != $T:
-        if injectionsTblSingleton.hasKey(clsDesc.signature):
-            return T(injectionsTblSingleton[clsDesc.signature])
-        elif injectionsTbl.hasKey(clsDesc.signature) :
-            let callProc = injectionsTbl[clsDesc.signature]
-            return T(callProc())
-        else:
-            raise newException(InjectionError, NOT_FOUND_ERROR_1 & $clsDesc)
+        withLock classObjTblLock:
+            if injectionsTblSingleton.hasKey(clsDesc.signature):
+                return T(injectionsTblSingleton[clsDesc.signature])
+            elif injectionsTbl.hasKey(clsDesc.signature) :
+                let callProc = injectionsTbl[clsDesc.signature]
+                return T(callProc())
+            else:
+                raise newException(InjectionError, NOT_FOUND_ERROR_1 & $clsDesc)
     elif T is ClassStaticObj and $ClassStaticObj != $T:
-        if staticInjectionsTblSingleton.hasKey(clsDesc.signature):
-            return cast[T](cast[ptr T](staticInjectionsTblSingleton[clsDesc.signature])[])
-        elif staticInjectionsTbl.hasKey(clsDesc.signature) :
-            let callProc = staticInjectionsTbl[clsDesc.signature]
-            return cast[T](cast[ptr T](callProc())[])
-        else:
-            raise newException(InjectionError, NOT_FOUND_ERROR_1 & $clsDesc)
+        withLock classStaticObjTblLock:
+            if staticInjectionsTblSingleton.hasKey(clsDesc.signature):
+                return cast[T](cast[ptr T](staticInjectionsTblSingleton[clsDesc.signature])[])
+            elif staticInjectionsTbl.hasKey(clsDesc.signature) :
+                let callProc = staticInjectionsTbl[clsDesc.signature]
+                return cast[T](cast[ptr T](callProc())[])
+            else:
+                raise newException(InjectionError, NOT_FOUND_ERROR_1 & $clsDesc)
     else:
         raise newException(InjectionError, $T & SUBCLASS_ERROR2)
 
 proc isInjectable*[T: ClassObj | ClassStaticObj](clsDesc : typedesc[T]) : bool =
+    ## Returns true if an object or a procedure of type ClassObj or ClassStaticObj exists in the tables otherwise false.
+    ##
     when T is ClassObj and $ClassObj != $T:
-        return injectionsTblSingleton.hasKey(clsDesc.signature) or injectionsTbl.hasKey(clsDesc.signature)
+        withLock classObjTblLock:
+            return injectionsTblSingleton.hasKey(clsDesc.signature) or injectionsTbl.hasKey(clsDesc.signature)
     elif T is ClassStaticObj and $ClassStaticObj != $T:
-        return staticInjectionsTblSingleton.hasKey(clsDesc.signature) or staticInjectionsTbl.hasKey(clsDesc.signature)
+        withLock classStaticObjTblLock:
+            return staticInjectionsTblSingleton.hasKey(clsDesc.signature) or staticInjectionsTbl.hasKey(clsDesc.signature)
     else:
         raise newException(InjectionError, $T & SUBCLASS_ERROR2)
 
 
 proc isSingleton*[T: ClassObj | ClassStaticObj](clsDesc : typedesc[T]) : bool =
+    ## Returns true if an object of type ClassObj or ClassStaticObj exists in the tables otherwise false.
+    ##
     when T is ClassObj and $ClassObj != $T:
-        return injectionsTblSingleton.hasKey(clsDesc.signature)
+        withLock classObjTblLock:
+            return injectionsTblSingleton.hasKey(clsDesc.signature)
     elif T is ClassStaticObj and $ClassStaticObj != $T:
-        return staticInjectionsTblSingleton.hasKey(clsDesc.signature)
+        withLock classStaticObjTblLock:
+            return staticInjectionsTblSingleton.hasKey(clsDesc.signature)
     else:
         raise newException(InjectionError, $T & SUBCLASS_ERROR2)
 
 proc resetInjectTbl*() =
-    injectionsTblSingleton.clear
-    injectionsTbl.clear
-    staticInjectionsTblSingleton.clear
-    staticInjectionsTbl.clear
+    ## Resets and removes all entries in the injection and injectors tables.
+    ##
+    withLock classObjTblLock:
+        injectionsTblSingleton.clear
+        injectionsTbl.clear
+    withLock classStaticObjTblLock:
+        staticInjectionsTblSingleton.clear
+        staticInjectionsTbl.clear
